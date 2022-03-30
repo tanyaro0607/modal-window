37db86e02db24cc6399581cd853b35133dd09f53
tanyaro0607 <ta.rohlova@yandex.ru>
2022-03-30 13:41:38 +0300

    create modal-window

---
+++ plugins/modal.js
@@ -0,0 +1,34 @@
+function _createModal(options) {
+    const modal = document.createElement('div')
+    modal.classList.add('wmodal')
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-overlay">
+            <div class="modal-window">
+                <div class="modal-header">
+                    <span class="modal-title">Modal title</span>
+                    <span class="modal-close">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="modal-footer">
+                    <button>Ok</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal)
+    return modal
+}
+
+$.modal = function(options) {
+    const $modal = _createModal(options)
+
+    return {
+        open() {},
+        close() {},
+        destroy() {}
+    }
+}
