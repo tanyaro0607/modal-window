0add5ea6ab92569a2b9003e4fcb1d371315a8c63
tanyaro0607 <ta.rohlova@yandex.ru>
2022-04-04 16:21:57 +0300

    отрисовывваю список с помощью метода render

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ fruits = [
     {id: 1, title: 'Манго', price: 40, img: 'https://images.pexels.com/photos/7543212/pexels-photo-7543212.jpeg?auto=compress&cs=tinysrgb&h=750&w=12600'},
 ]
 
+const toHTML = fruit => `
+    <div class="col">
+        <div class="card">
+            <img class="card-img-top" style="height: 300px" src="${fruit.img}" alt="${fruit.title}">
+            <div class="card-body">
+                <h5 class="card-title">${fruit.title}</h5>
+                <a href="#" class="btn btn-primary">Посмотреть цену</a>
+                <a href="#" class="btn btn-danger">Удалить</a>
+            </div>
+        </div>
+    </div>
+`
+
+//рендерит список
+function render() {
+    const html = fruits.map(toHTML).join('') 
+    document.querySelector('#fruits').innerHTML = html
+}
+
+render()
+
 const modal = $.modal({
     title: 'Tatiana Modal',
     closable: true,
